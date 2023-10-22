d123e60c71846b2a121cbb16fb0e64e1f8c2b4ad
abdullahnaeemmalik <97474332+abdullahnaeemmalik@users.noreply.github.com>
2023-10-22 19:13:22 -0400

    Update X_n given V.py

--- files/X_n given V.py
+++ files/X_n given V.py
@@ -1,4 +1,5 @@
-V = [1, 2, 4]
+#take, for example, the following set of vertices:
+V = [1,2,3]
 
 def X(n):
     if n == 0:
@@ -8,4 +9,5 @@ def X(n):
         X = [r + [v] for r in X for v in V if r[-1] <= v]
     return(X)
 
-return(X(2))
+X(2)
+#returns all 2-simplices, including degenerate ones.
